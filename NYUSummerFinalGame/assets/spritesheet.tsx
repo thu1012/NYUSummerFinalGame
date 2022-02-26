f14e3c1bbf7f168327f8cf09f789fb817e864124
Tairan Hu <40744575+thu1012@users.noreply.github.com>
2022-02-25 19:59:07 -0600

    Add files via upload

---
+++ NYUSummerFinalGame/assets/spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="spritesheet.png" width="306" height="138"/>
+</tileset>
